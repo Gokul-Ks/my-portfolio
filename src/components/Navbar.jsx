7785b5d9ebf832e9b40ce7c6e8fd330e35ff629c
Gokul_Zuzu <gokul.19ec@kct.ac.in>
2022-11-23 11:39:42 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://drive.google.com/file/d/1eT9Vmc7eRuOxHUdg9YTTyWB6dV-sEfVD/view?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
